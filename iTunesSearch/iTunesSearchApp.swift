28e97c9f4421ee2b2b00f189b3e6e73254669e17
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-06-03 00:06:28 +0800

    Initial Commit

---
+++ iTunesSearch/iTunesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchApp.swift
+//  iTunesSearch
+//
+//  Created by Failyn Kaye Sedik on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
