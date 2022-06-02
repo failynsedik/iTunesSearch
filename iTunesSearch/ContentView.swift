39e1518d76f2f4532251f682e3b0dfd2a2916e26
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-06-03 00:28:45 +0800

    Add TabView

--- iTunesSearch/ContentView.swift
+++ iTunesSearch/ContentView.swift
@@ -8,10 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+	enum TabItem: String {
+		case music
+		case movies
+		case search
+		case tones
+		case more
+		
+		var title: String {
+			return self.rawValue.capitalized
+		}
+		
+		var iconName: String {
+			switch self {
+			case .music: return "music.note.list"
+			case .movies: return "film.fill"
+			case .search: return "magnifyingglass"
+			case .tones: return "bell.fill"
+			case .more: return "ellipsis"
+			}
+		}
+	}
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		TabView {
+			music
+			movies
+			search
+			tones
+			more
+		}
     }
+	
+	var music: some View {
+		DummyView()
+			.tabItem {
+				Image(systemName: TabItem.music.iconName)
+				Text(TabItem.music.title)
+			}
+	}
+	
+	var movies: some View {
+		DummyView()
+			.tabItem {
+				Image(systemName: TabItem.movies.iconName)
+				Text(TabItem.movies.title)
+			}
+	}
+	
+	var search: some View {
+		DummyView()
+			.tabItem {
+				Image(systemName: TabItem.search.iconName)
+				Text(TabItem.search.title)
+			}
+	}
+	
+	var tones: some View {
+		DummyView()
+			.tabItem {
+				Image(systemName: TabItem.tones.iconName)
+				Text(TabItem.tones.title)
+			}
+	}
+	
+	var more: some View {
+		DummyView()
+			.tabItem {
+				Image(systemName: TabItem.more.iconName)
+				Text(TabItem.more.title)
+			}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ iTunesSearch/Modules/DummyView.swift
@@ -0,0 +1,14 @@
+//
+//  DummyView.swift
+//  iTunesSearch
+//
+//  Created by Failyn Kaye Sedik on 6/3/22.
+//
+
+import SwiftUI
+
+struct DummyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
