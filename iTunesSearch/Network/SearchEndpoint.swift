996b228a3be5de20c935477c70c2067501091f76
Failyn Kaye Sedik <failynsedik@gmail.com>
2022-06-03 00:13:33 +0800

    Add SearchEndpoint

---
+++ iTunesSearch/Network/SearchEndpoint.swift
@@ -0,0 +1,43 @@
+//
+//  SearchEndpoint.swift
+//  iTunesSearch
+//
+//  Created by Failyn Kaye Sedik on 6/3/22.
+//
+
+import ServiceKit
+
+/// NOTE: Should hold endpoints related to iTunes searches.
+enum SearchEndpoint {
+	case getMovies(term: String, offset: Int)
+}
+
+extension SearchEndpoint: EndpointProtocol {
+	var host: String { "itunes.apple.com" }
+	
+	var path: String {
+		switch self {
+		case .getMovies: return "/search"
+		}
+	}
+	
+	var httpMethod: EndpointRequestMethod {
+		switch self {
+		case .getMovies: return .get
+		}
+	}
+	
+	var parameters: [String: Any] {
+		switch self {
+		case let .getMovies(term, offset):
+			let parameters: [String: Any] = [
+				"term": term,
+				"offset": offset,
+				"limit": 10,
+				"country": "au", // Default as per requirement
+				"media": "movie"//"movie&;all"// Default as per requirement
+			]
+			return parameters
+		}
+	}
+}
